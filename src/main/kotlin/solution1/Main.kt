3c4c2a4e6352c4c7821e2ce983fa90a71ec1d90a
michal_powloka <michal.powloka@accenture.com>
2017-12-06 13:56:11 +0100

    Extracted captcha from file and converted it into CircularList

--- src/main/kotlin/solution1/Main.kt
+++ src/main/kotlin/solution1/Main.kt
@@ -1,5 +1,16 @@
 package solution1
 
-fun main(args: Array<String>) {
+import common.CircularList
+import java.io.FileReader
 
+fun main(args: Array<String>) {
+    val captchaList = extractCaptcha()
+}
+
+fun extractCaptcha(): CircularList<Int> {
+    val result = CircularList<Int>(ArrayList())
+    val reader = FileReader("src\\main\\resources\\solution1-captcha.txt")
+    reader.readText().toCharArray().forEach { result.add( Character.getNumericValue(it) ) }
+    reader.close()
+    return result
 }
