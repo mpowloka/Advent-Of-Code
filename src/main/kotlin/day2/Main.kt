c69f257bcd4630d1a6e501a435cb5509a1132b11
michal_powloka <michal.powloka@accenture.com>
2017-12-08 11:12:19 +0100

    Solved day 2 riddle, both parts. Four stars in total!

--- src/main/kotlin/day2/Main.kt
+++ src/main/kotlin/day2/Main.kt
@@ -5,8 +5,41 @@ import java.io.FileReader
 
 fun main(args: Array<String>) {
 
-    loadSpreadsheet()
+    val spreadsheet = loadSpreadsheet()
 
+    val checkSum = calculateChecksum(spreadsheet)
+    println("Final checksum: $checkSum")
+
+    var result = 0
+    spreadsheet.forEach{result += getRowResult(it)}
+    println("Final second result: $result")
+}
+
+private fun getRowResult(row: List<Int>): Int {
+
+    for(number in row) {
+        var rowResult = -1
+        row.find {
+            if(number % it == 0 && number != it) {
+                rowResult = number / it
+                println("Found a pair: $number and $it. Result is: $rowResult")
+            }
+            number % it == 0 && number != it
+        }
+        return rowResult
+    }
+    return -2
+}
+
+private fun calculateChecksum(spreadsheet: List<List<Int>>): Int {
+    var checkSum = 0
+    spreadsheet.forEach {
+        println("Max in the row: ${it.max()}, min in the row: ${it.min()}")
+        println("Adding: ${(it.max() ?: 0) - (it.min() ?: 0)} to checksum")
+        checkSum += (it.max() ?: 0) - (it.min() ?: 0)
+        println("Current checksum: $checkSum \n")
+    }
+    return checkSum
 }
 
 private fun loadSpreadsheet(): List<List<Int>> {
@@ -18,7 +51,7 @@ private fun loadSpreadsheet(): List<List<Int>> {
         result.add(rowToAdd)
     }
 
-    println("Loaded spreadsheet:\n ${result.joinToString(separator = "\n")}")
+    println("Loaded spreadsheet:\n ${result.joinToString(separator = "\n")} \n")
 
     return result
 }
